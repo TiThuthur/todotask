342dac00c84d775ace676f7c51ebeb9816f5af15
TiThuthur <thurthur.petit.py@gmail.com>
2023-11-08 11:08:44 +0100

    prendre connaisance du commentaire dans "function"

--- asset/js/agent.js
+++ asset/js/agent.js
@@ -12,23 +12,22 @@ for (let i = 0; i < 50; i++) {
 }
 const animate = () => {
   context.clearRect(0, 0, width, height);
-  // for (let i = 0; i < agents.length; i++) {
-  //   const agent = agents[i];
-  //   for (let j = i+1; j < agents.length; j++) {
-  //     const other = agents[j];
-      
+  for (let i = 0; i < agents.length; i++) {
+    const agent = agents[i];
+    for (let j = i + 1; j < agents.length; j++) {
+      const other = agents[j];
 
+      const dist = agent.pos.distance(other.pos);
 
-    // }
+      console.log(Math.map(0, 200, 12, 1));
+      // context.lineWidth =
+    }
     agents.forEach((agent) => {
       agent.update();
       agent.draw(context);
       agent.bounce(width, height);
     });
-  
-
-
+  }
   window.requestAnimationFrame(animate);
 };
 animate();
-// };
--- asset/js/function.js
+++ asset/js/function.js
@@ -6,3 +6,22 @@ const getRandomInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
+/**
+ *  try to find the linearization of the value
+ *
+ * exemple : pour 200 on a 12
+ * pour 100 on a 8.33 (calcul faux mais s'appuyer sur l'exemple)
+ *
+ */
+// const distanceMap= (dist,max,outputMin,outputMax)=>{
+//   let output
+//   outputMax=12
+//   outputMin=1
+//   dist=100
+//   max=200
+
+//   if (dist > max) return 0
+
+//   output= dist/outputMax *outputMin
+
+// }
